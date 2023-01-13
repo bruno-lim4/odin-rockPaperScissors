a2c9f2beac341a3223702f4ddf7b2509b2b1abf5
Bruno Lima <brunofigueiredolima@gmail.com>
2023-01-13 10:45:42 -0300

    finished

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var playerPoints = 0, computerPoints = 0;
+
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors']
     let choice = options[Math.floor(Math.random() * options.length)]
@@ -11,30 +13,30 @@ function playRound(player, computer) {
 
     if (player == 'rock') {
         if (computer == 'paper') {
-            res = "You Lose! Paper beats Rock"
+            res = "<b>You Lose!</b> Paper beats Rock"
             point = -1
         } else {
-            res = "You Win! Rock beats Scissors"
+            res = "<b>You Win!</b> Rock beats Scissors"
             point = 1
         }
     }
 
     if (player == 'paper') {
         if (computer == 'rock') {
-            res = "You Win! Paper beats Rock"
+            res = "<b>You Win!</b> Paper beats Rock"
             point = 1
         } else {
-            res = "You Lose! Scissors beats Paper"
+            res = "<b>You Lose!</b> Scissors beats Paper"
             point = -1
         }
     }
 
     if (player == 'scissors') {
         if (computer == 'paper') {
-            res = "You Win! Scissors beats Paper"
+            res = "<b>You Win!</b> Scissors beats Paper"
             point = 1
         } else {
-            res = "You Lose! Rock beats Scissors"
+            res = "<b>You Lose!</b> Rock beats Scissors"
             point = -1
         }
     }
@@ -47,30 +49,79 @@ function playRound(player, computer) {
     return [res, point] // 1 - player win   -1 - pc wins   0 - tie
 }
 
-function game() {
-    let userp = 0, computerp = 0
+function hideResult() {
+    const div = document.querySelector('.result');
+    if (!div) return;
+    div.textContent = "";
+}
 
-    for(let i = 0; i < 5; i++) {
-        let result = playRound(prompt("Type: Paper, Rock, or Scissors").toLowerCase(), getComputerChoice())
-
-        console.log(result[0])
-
-        if (result[1] == 1) {
-            userp++;
-        } else if (result[1] == -1) {
-            computerp++;
-        }
-    }
-
-    console.log("Final Score: \nYou: " + userp + " points\n" + "Computer: " + computerp + " points");
-    
-    if (userp > computerp) {
-        console.log("YOU WIN")
-    } else if (computerp > userp) {
-        console.log("YOU LOSE")
+function showResult() {
+    if (!document.querySelector('.result')) {
+        let div = document.createElement('div');
+        div.classList.add('result');
+        div.textContent = playerPoints==5? "YOU WIN" : "YOU LOSE";
+        document.querySelector('.cards').append(div);
     } else {
-        console.log("TIE")
+        const div = document.querySelector('.result');
+        div.textContent = playerPoints==5? "YOU WIN" : "YOU LOSE";
     }
 }
 
-game()
+function showMessage(pcChoice, message) {
+    if (!document.querySelector('.message')) {
+        let div = document.createElement('div');
+        div.classList.add('message');
+        div.innerHTML = `Computer choice: ${pcChoice} <br> ${message}`
+        document.querySelector('.cards').append(div);
+    } else {
+        const div = document.querySelector('.message');
+        div.innerHTML = `Computer choice: ${pcChoice} <br> ${message}`
+    }
+}
+
+function updateScore() {
+    const playerScore = document.querySelector('#you');
+    const computerScore = document.querySelector('#pc');
+
+    playerScore.textContent = playerPoints;
+    computerScore.textContent = computerPoints;
+}
+
+function processClick(e) {
+    let player_choice = e.target.dataset.choice;
+
+    if (!player_choice) return;
+
+    if (playerPoints == 0 && computerPoints == 0) {
+        hideResult();
+    }
+
+    let pcChoice = getComputerChoice();
+
+    let result = playRound(player_choice, pcChoice);
+
+    console.log(result);
+
+    if (result[1] == -1) {
+        computerPoints++;
+        if (computerPoints == 5) {
+            showResult();
+            computerPoints = 0;
+            playerPoints = 0;
+        }
+    } else if (result[1] == 1) {
+        playerPoints++;
+        if (playerPoints == 5) {
+            showResult();
+            computerPoints = 0;
+            playerPoints = 0;
+
+        }
+    }
+
+    console.log(playerPoints);
+    updateScore();
+    showMessage(pcChoice, result[0]);
+}
+
+document.addEventListener('click', processClick);
